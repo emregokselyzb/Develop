f23531fe3c18443a9ad1ecdd3c361bbe6bee8e80
emregokselyzb <emre.g.yzb@gmail.com>
2023-08-02 16:22:20 +0300

    Java Review

--- Lambda/src/AppleHeavyPredicate.java
+++ Lambda/src/AppleHeavyPredicate.java
@@ -1,7 +1,6 @@
 public class AppleHeavyPredicate implements ApplePredicate{
     @Override
     public boolean test(Apple apple) {
-        return apple.getWeight()>20
-        0;
+        return apple.getWeight()>200;
     }
 }
