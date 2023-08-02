6daaa5862c80f6bf64c1e9fac2e496f9a45dae54
emregokselyzb <emre.g.yzb@gmail.com>
2023-08-02 18:38:58 +0300

    Java Review

---
+++ src/Hola/HelloWorld.java
@@ -0,0 +1,15 @@
+package Hola;
+
+public class HelloWorld {
+
+   static int a=25;
+
+    public static void main(String[] args) {
+        System.out.println("Hello and Hola");
+
+
+        System.out.println(a++);
+
+        System.out.println(++a);
+    }
+}
