f09bdf8da47bc6af3206ac569906e85ac184f94d
emregokselyzb <emre.g.yzb@gmail.com>
2023-08-02 18:41:04 +0300

    Java Review

--- Lambda/src/Hello.java
+++ Lambda/src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello and Hola Gracias");
+        System.out.println("Hello and Hola Graciass");
+        System.out.println("Hadi bakalım");
     }
 }
